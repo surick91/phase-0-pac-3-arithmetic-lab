700b0c137b5ed0a63a03c4d4ef8c624289124bf2
sam <surickj@gmail.com>
2021-04-13 16:17:33 -0700

    done

---
+++ index.js
@@ -0,0 +1,19 @@
+function add(){
+    return 1+80;
+}
+function subtract(){
+    return 60-40;
+}
+function multiply(){
+    return 2 * 3.4;
+} 
+function divide(){
+    return 5.0 / 2.5; 
+}
+function add(number){
+    return number+=5;
+}
+function makeInt(n,radix){
+    return parseInt(n, radix)
+}
+makeInt("", 10)
